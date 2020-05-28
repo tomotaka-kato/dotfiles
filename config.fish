b8f0f194d042971c192cf8c5424de3f6b7359fa3
kato.tomotaka@smileforce.jp <kato.tomotaka@smileforce.jp>
2020-05-29 07:51:56 +0900

    pythonの設定追加

--- config.fish
+++ config.fish
@@ -18,6 +18,7 @@ set -x JENV_ROOT /usr/local/var/jenv
 
 # pyenv
 status --is-interactive; and source (pyenv init -|psub)
+source (pyenv virtual-env init -|psub)
 
 # alias
 alias vim nvim
@@ -30,17 +31,3 @@ set -x PATH $GOPATH/bin $PATH
 # dotnet-sdkの情報を共有しない
 set -x DOTNET_CLI_TELEMETRY_OPTOU 1
 
-
-# PROOFIT
-set -x ASPNETCORE_ENVIRONMENT development
-set -x DataSourceSettings__MedicineImageApiBaseUrl https://proofitclouddev.azure-api.net/OdpFunctions-dev2/v1.0/medicine/image
-set -x DataSourceSettings__PackageDetailApiBaseUrl https://proofitclouddev.azure-api.net/OdpFunctions-dev2/v1.0/package/detail?serialCode={serialCode}
-set -x DataSourceSettings__FaqPageUrl https://proofitcloudpocdev.z11.web.core.windows.net/faq/
-set -x DataSourceSettings__LicensePageUrl https://proofitcloudpocdev.z11.web.core.windows.net/osslicense/
-set -x DataSourceSettings__TosPageUrl https://proofitcloudpocdev.z11.web.core.windows.net/tos
-
-# 需要予測
-set -x FILE_SAVER_OUTPUT_DIRECTORY $HOME/Documents/tmp/out
-set -x FILE_SAVER_READ_DIRECTORY $HOME/Documents/tmp/read
-set -x FILE_SAVER_DB_DIRECTORY $HOME/Documents/tmp/database
-set -x FILE_SAVER_DB_URL 'jdbc:sqlite:/Users/tomo/Documents/tmp/database/database.sqlite'
