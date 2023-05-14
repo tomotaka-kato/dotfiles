35ce0c356a4d5ecb09f8de9f2bba49d1416b3e8c
tomo <tomotaka.k612@gmail.com>
2023-05-14 23:37:25 +0900

    modify

--- startup.ps1
+++ startup.ps1
@@ -1,5 +1,5 @@
 # start AutoHotKey
-Start-Process -FilePath $HOME/scoop/shims/AutoHotKey -ArgumentList $HOME/dotfiles/AutoHotkey/hightlight_active_window_border.ahk
+# Start-Process -FilePath $HOME/scoop/shims/AutoHotKey -ArgumentList $HOME/dotfiles/AutoHotkey/hightlight_active_window_border.ahk
 
  # start GlazeWM
  GlazeWM --config = $HOME/dotfiles/glazewm/config.yaml
