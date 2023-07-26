d8f750dc7627287a78ff5450ff491c6a1ae298fd
tomo <tomotaka.k612@gmail.com>
2023-07-26 11:23:39 +0900

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -92,6 +92,51 @@ config.keys = {
     key = 'v',
     mods = 'LEADER',
     action = wezterm.action{SplitVertical={domain="CurrentPaneDomain"}},
+  },
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = wezterm.action{ActivatePaneDirection="Left"},
+  },
+  {
+    key = 'j',
+    mods = 'LEADER',
+    action = wezterm.action{ActivatePaneDirection="Down"},
+  },
+  {
+    key = 'k',
+    mods = 'LEADER',
+    action = wezterm.action{ActivatePaneDirection="Up"},
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = wezterm.action{ActivatePaneDirection="Right"},
+  },
+  {
+    key = 'w',
+    mods = 'LEADER',
+    action = wezterm.action{CloseCurrentPane={confirm=true}},
+  },
+  {
+    key = 'H',
+    mods = 'LEADER',
+    action = wezterm.action{AdjustPaneSize={"Left", 5}},
+  },
+  {
+    key = 'J',
+    mods = 'LEADER',
+    action = wezterm.action{AdjustPaneSize={"Down", 5}},
+  },
+  {
+    key = 'K',
+    mods = 'LEADER',
+    action = wezterm.action{AdjustPaneSize={"Up", 5}},
+  },
+  {
+    key = 'L',
+    mods = 'LEADER',
+    action = wezterm.action{AdjustPaneSize={"RIGHT", 5}},
   }
 }
 
