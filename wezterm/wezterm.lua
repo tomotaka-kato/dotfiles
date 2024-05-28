974438cbd3439342f960e63e79143321a66d59dd
Tomotaka Kato <tomotaka.k612@gmail.com>
2024-05-28 15:20:08 +0900

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -28,63 +28,74 @@ config.hide_tab_bar_if_only_one_tab = true
 if is_windows then
     config.font = wezterm.font( 'HackGen Console NF')
 else
-    config.font = wezterm.font( 'HackGenNerd Console')
+    config.font = wezterm.font( 'PlemolJP Console')
 end
-config.font_rules = {
-  -- For Bold-but-not-italic text, use this relatively bold font, and override
-  -- its color to a tomato-red color to make bold text really stand out.
-  {
-    intensity = 'Bold',
-    italic = false,
-    font = wezterm.font_with_fallback{ 
-      family = config.font,
-      -- Override the color specified by the terminal output and force
-      -- it to be tomato-red.
-      -- The color value you set here can be any CSS color name or
-      -- RGB color string.
-      -- { foreground = 'tomato' }
-     },
-  },
-
-  -- Bold-and-italic
-  {
-    intensity = 'Bold',
-    italic = true,
-    font = wezterm.font_with_fallback {
-      family = 'HackGen Console NF',
-      italic = true,
-    },
-  },
-
-  -- normal-intensity-and-italic
-  {
-    intensity = 'Normal',
-    italic = true,
-    font = wezterm.font_with_fallback {
-      family = 'HackGen Console NF',
-      italic = true,
-    },
-  },
-
-  -- half-intensity-and-italic (half-bright or dim); use a lighter weight font
-  {
-    intensity = 'Half',
-    italic = true,
-    font = wezterm.font_with_fallback {
-      family = 'HackGen Console NF',
-      italic = true,
-    },
-  },
-
-  -- half-intensity-and-not-italic
-  {
-    intensity = 'Half',
-    italic = false,
-    font = wezterm.font_with_fallback {
-      family = 'HackGen Console NF'
-    },
-  },
-}
+-- config.font_rules = {
+--   -- For Bold-but-not-italic text, use this relatively bold font, and override
+--   -- its color to a tomato-red color to make bold text really stand out.
+--   {
+--     intensity = 'Bold',
+--     italic = false,
+--     font = wezterm.font_with_fallback{ 
+--       family = config.font,
+--       -- Override the color specified by the terminal output and force
+--       -- it to be tomato-red.
+--       -- The color value you set here can be any CSS color name or
+--       -- RGB color string.
+--       -- { foreground = 'tomato' }
+--      },
+--   },
+--
+--   -- Bold
+--   {
+--     intensity = 'Bold',
+--     italic = false,
+--     font = wezterm.font_with_fallback {
+--       family = config.font,
+--       italic = false,
+--     },
+--   },
+--
+--   -- Bold-and-italic
+--   {
+--     intensity = 'Bold',
+--     italic = true,
+--     font = wezterm.font_with_fallback {
+--       family = config.font,
+--       italic = true,
+--       weight = 'Bold',
+--     },
+--   },
+--
+--   -- normal-intensity-and-italic
+--   {
+--     intensity = 'Normal',
+--     italic = true,
+--     font = wezterm.font_with_fallback {
+--       family = config.font,
+--       italic = true,
+--     },
+--   },
+--
+--   -- half-intensity-and-italic (half-bright or dim); use a lighter weight font
+--   {
+--     intensity = 'Half',
+--     italic = true,
+--     font = wezterm.font_with_fallback {
+--       family = config.font,
+--       italic = true,
+--     },
+--   },
+--
+--   -- half-intensity-and-not-italic
+--   {
+--     intensity = 'Half',
+--     italic = false,
+--     font = wezterm.font_with_fallback {
+--       family = config.font
+--     },
+--   },
+-- }
 config.cell_width = 1.2
 config.adjust_window_size_when_changing_font_size = false
 config.line_height = 1.5
