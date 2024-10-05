a2220dd00b82c485df2cf609fc19546da2f3a2db
tomo <tomotaka.k612@gmail.com>
2024-10-05 21:25:56 +0900

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -56,8 +56,18 @@ config.audible_bell = "Disabled"
 
 local opacity = 1
 if is_windows then
-	opacity = 0.85
-	config.win32_system_backdrop = "Acrylic"
+	-- opacity = 0.85
+	-- config.win32_system_backdrop = "Acrylic"
+	config.window_background_image = "~/dotfiles/wezterm/images/towa1.jpg"
+	config.window_background_image_hsb = {
+		-- Darken the background image by reducing it to 1/3rd
+		brightness = 0.3,
+		-- You can adjust the hue by scaling its value.
+		-- a multiplier of 1.0 leaves the value unchanged.
+		hue = 1.0,
+		-- You can adjust the saturation also.
+		saturation = 1.0,
+	}
 else
 	opacity = 0.9
 	config.macos_window_background_blur = 30
